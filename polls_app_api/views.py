f520990c3d74e1f4012c99913290024cdcf7689f
Mikhail Sergeev <m.m.sergeev1@gmail.com>
2020-10-28 18:35:29 +0300

    some refactoring + README.md

--- polls_app_api/views.py
+++ polls_app_api/views.py
@@ -1,6 +1,6 @@
 import logging
 from django.contrib.auth.models import User
-from django.utils import timezone
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -21,13 +21,12 @@ class RegisteredVoteDetailView(APIView):
             registered_votes = RegisteredVote.objects.filter(user=user)
             logging.debug(f'registered votes = {registered_votes}')
             serializer = RegisteredVoteDetailSerializer(registered_votes, many=True)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except (User.DoesNotExist, polls_app.models.RegisteredVote.DoesNotExist):
             logging.debug(f"anonymous_user_id = {pk}")
-            try:
-                registered_votes = RegisteredVote.objects.filter(anonymous_user_id=pk)
-                logging.debug(f'registered votes = {registered_votes}')
-                serializer = RegisteredVoteDetailSerializer(registered_votes, many=True)
-                return Response(serializer.data)
-            except polls_app.models.RegisteredVote.DoesNotExist:
-                return Response({"message": "Нет опросов с таким ID пользователя"})
+            registered_votes = RegisteredVote.objects.filter(anonymous_user_id=pk)
+            if len(registered_votes) == 0:
+                return Response({"message": "Нет опросов с таким ID пользователя"}, status=status.HTTP_404_NOT_FOUND)
+            logging.debug(f'registered votes = {registered_votes}')
+            serializer = RegisteredVoteDetailSerializer(registered_votes, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
